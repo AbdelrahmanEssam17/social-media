e65b5d52fc23ddb9bf32345914114c58b7820754
”abdelrahman <eabdo6990@gmail.com>
2026-09-23 19:39:42 +0300

    made the folder structure and routing

---
+++ social-media/src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import MainLayout from "./Layouts/MainLayout";
+import AuthLayout from "./Layouts/AuthLayout";
+import login from "./Pages/Login";
+import Feedpage from "./Pages/Feedpage";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
+import PostDetails from "./Pages/PostDetails";
+import Register from "./Pages/Register";
+import NotFound from "./Pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Feedpage /> },
+      { path: "profile", element: <Profile /> },
+      { path: "post-details", element: <PostDetails /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ social-media/src/Pages/FeedPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Feedpage() {
+  return <div className="p-8 max-w-7xl mx-auto"></div>;
+}
